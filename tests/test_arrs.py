004336d3b878c623e6ce0970ba2d32b7791b5352
Dmitrii Degtiarev <9774385859@mail.ru>
2023-02-01 20:15:01 +0300

    Написаны тесты на unittest

--- tests/test_arrs.py
+++ tests/test_arrs.py
@@ -1,14 +1,14 @@
+import unittest
 from utils import arrs
 
 
-def test_get():
-    assert arrs.get([1, 2, 3], 2, "test") == 3
-    assert arrs.get([], -1, "test") == "test"
+class TestArrs(unittest.TestCase):
+    def test_get(self):
+        self.assertEqual(arrs.get([1, 2, 3], 2, "test"), 3)
+        self.assertEqual(arrs.get([], -1, "test"), 'test')
+
+    def test_my_slice(self):
+        self.assertEqual(arrs.my_slice([1, 2, 3, 4], 1, 3), [2, 3])
+        self.assertEqual(arrs.my_slice([1, 2, 3], 1), [2, 3])
 
 
-def test_slice():
-    assert arrs.my_slice([1, 2, 3, 4], 1, 3) == [2, 3]
-    assert arrs.my_slice([1, 2, 3], 1) == [2, 3]
-    assert arrs.my_slice([]) == []
-    assert arrs.my_slice([1, 2, 3, 4], -7) == [1, 2, 3, 4]
-    assert arrs.my_slice([1, 2, 3, 4], -2) == [3, 4]
